4c3fe21b73deffc17d33564be254afa7b521f2e0
Denis Dragan <serkis-denis.dragan@ogr.iuc.edu.tr>
2023-06-19 12:14:02 +0300

    Fixed

---
+++ Pods/Target Support Files/FirebaseFirestore/FirebaseFirestore-umbrella 2.h
@@ -0,0 +1,65 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "FIRAggregateQuery 2.h"
+#import "FIRAggregateQuery 3.h"
+#import "FIRAggregateQuery.h"
+#import "FIRAggregateQuerySnapshot 3.h"
+#import "FIRAggregateQuerySnapshot.h"
+#import "FIRAggregateSource 3.h"
+#import "FIRAggregateSource.h"
+#import "FIRCollectionReference 2.h"
+#import "FIRCollectionReference 3.h"
+#import "FIRCollectionReference.h"
+#import "FIRDocumentChange.h"
+#import "FIRDocumentReference.h"
+#import "FIRDocumentSnapshot.h"
+#import "FirebaseFirestore.h"
+#import "FIRFieldPath 3.h"
+#import "FIRFieldPath.h"
+#import "FIRFieldValue 3.h"
+#import "FIRFieldValue.h"
+#import "FIRFilter 2.h"
+#import "FIRFilter 3.h"
+#import "FIRFilter.h"
+#import "FIRFirestore.h"
+#import "FIRFirestoreErrors 3.h"
+#import "FIRFirestoreErrors.h"
+#import "FIRFirestoreSettings 3.h"
+#import "FIRFirestoreSettings.h"
+#import "FIRFirestoreSource 2.h"
+#import "FIRFirestoreSource 3.h"
+#import "FIRFirestoreSource.h"
+#import "FIRGeoPoint.h"
+#import "FIRListenerRegistration.h"
+#import "FIRLoadBundleTask.h"
+#import "FIRLocalCacheSettings 3.h"
+#import "FIRLocalCacheSettings.h"
+#import "FIRQuery 2.h"
+#import "FIRQuery.h"
+#import "FIRQuerySnapshot 2.h"
+#import "FIRQuerySnapshot 3.h"
+#import "FIRQuerySnapshot.h"
+#import "FIRSnapshotMetadata 2.h"
+#import "FIRSnapshotMetadata 3.h"
+#import "FIRSnapshotMetadata.h"
+#import "FIRTimestamp 2.h"
+#import "FIRTimestamp 3.h"
+#import "FIRTimestamp.h"
+#import "FIRTransaction.h"
+#import "FIRTransactionOptions 3.h"
+#import "FIRTransactionOptions.h"
+#import "FIRWriteBatch.h"
+
+FOUNDATION_EXPORT double FirebaseFirestoreVersionNumber;
+FOUNDATION_EXPORT const unsigned char FirebaseFirestoreVersionString[];
+
